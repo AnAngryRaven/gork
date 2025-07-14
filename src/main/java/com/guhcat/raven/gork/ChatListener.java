7bf3059ec0f1c2794decfbf6a19a13aa62b10e47
anangryraven <39865912+AnAngryRaven@users.noreply.github.com>
2025-07-14 10:19:44 -0400

    fix #1

--- src/main/java/com/guhcat/raven/gork/ChatListener.java
+++ src/main/java/com/guhcat/raven/gork/ChatListener.java
@@ -1,22 +1,16 @@
 package com.guhcat.raven.gork;
 
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.server.BroadcastMessageEvent;
-import org.bukkit.scheduler.BukkitScheduler;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class ChatListener implements Listener {
 
 
     @EventHandler
     public void onChat(AsyncPlayerChatEvent event) {
-        Gork.VARIANTS.forEach((variant) ->{
+        for(String variant : Gork.VARIANTS){
             if(event.getMessage().startsWith(Gork.PREFIX + " " +variant)){
                 String playerName = event.getPlayer().getName();
                 Bukkit.getScheduler().runTaskLaterAsynchronously(Gork.plugin, () ->{
@@ -28,7 +22,8 @@ public class ChatListener implements Listener {
                     });
                     Gork.plugin.getLogger().log(java.util.logging.Level.INFO, "<" + Gork.BotName +"> @"+playerName+" yeh");
                 }, 10);
+                break;
             }
-        });
+        }
     }
 }
